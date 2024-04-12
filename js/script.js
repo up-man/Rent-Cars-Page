29aab67723a3e5f3f189a989fb3923f31c6084e4
Andrey <123223390+up-man@users.noreply.github.com>
2024-04-12 15:09:08 +0700

    First Commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Swiper slider
+const swiper = new Swiper("#swiper-brands", {
+  freeMode: true,
+
+  slidesPerView: "auto",
+});
+
+const swiper1 = new Swiper("#swiper-testimonials", {
+  // loop: true,
+  freeMode: true,
+  slidesPerView: "auto",
+});
