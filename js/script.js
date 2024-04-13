3622d57fc6995f3a5f118eb5ece44cc04a3de1fc
Andrey <123223390+up-man@users.noreply.github.com>
2024-04-13 17:08:00 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 // Swiper slider
-const swiper = new Swiper("#swiper-brands", {
+const swiper1 = new Swiper("#swiper-brands", {
   freeMode: true,
 
   slidesPerView: "auto",
 });
+const swiper2 = new Swiper("#swiper-popular", {
+  loop: true,
+  slidesPerView: 1,
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+    1160: {
+      slidesPerView: 4,
+    },
+  },
+});
 
-const swiper1 = new Swiper("#swiper-testimonials", {
-  // loop: true,
+const swiper3 = new Swiper("#swiper-testimonials", {
   freeMode: true,
   slidesPerView: "auto",
 });
